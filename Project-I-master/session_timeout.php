5d8da48fdde144a258fa7fe1b49e53dfc0d98710
Santosh Dahal <sirishsds@gmail.com>
2024-04-04 13:51:18 +0545

    Manage Unused File

--- Project-I-master/session_timeout.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 300)) {
-
-    header("location:logout_action.php?sessionExpired=true");
-    exit();
-}
-
-$_SESSION['LAST_ACTIVITY'] = time();
-?>
--- add_beneficiary_action.php
+++ add_beneficiary_action.php
@@ -100,5 +100,4 @@ if ($result->num_rows > 0) {
 
 
 </body>
-
 </html>
